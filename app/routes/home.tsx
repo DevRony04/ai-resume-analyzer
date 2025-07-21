ba9da72d58475486a2b1f59889103abce00fc9d5
Deepyaman Mondal <deepyamanmondal7@gmail.com>
2025-07-21 13:30:30 +0530

    Cloud Storage, Pdf-Image Conversion, key-value Storage, AI feedback

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,10 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect,useState} from "react";
+import {useNavigate} from "react-router";
+
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,6 +15,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+  
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar/>
     <section className="main-section">
